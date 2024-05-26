1e6fd9ec9f1a857b2d863c75a120d4800dba2d20
Zeck Lowe <lowezeck@gmail.com>
2024-05-27 01:47:08 +0800

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -17,28 +17,27 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ProviderScope(
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.dark,
-        darkTheme: ThemeData(
-          scaffoldBackgroundColor: Colors.black,
-          brightness: Brightness.dark,
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: Colors.white10,
-            type: BottomNavigationBarType.fixed,
-            selectedLabelStyle: TextStyle(
-              fontSize: 12,
+          debugShowCheckedModeBanner: false,
+          themeMode: ThemeMode.dark,
+          darkTheme: ThemeData(
+            scaffoldBackgroundColor: Colors.black,
+            brightness: Brightness.dark,
+            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+              backgroundColor: Colors.white10,
+              type: BottomNavigationBarType.fixed,
+              selectedLabelStyle: TextStyle(
+                fontSize: 12,
+              ),
+              unselectedLabelStyle: TextStyle(
+                fontSize: 12,
+              ),
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.white38,
             ),
-            unselectedLabelStyle: TextStyle(
-              fontSize: 12,
-            ),
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white38,
           ),
-        ),
-        home: MusicPlayerView(
-          id: 3,
-        ),
-      ),
+          home: MusicPlayerView(
+            id: 3,
+          )),
     );
   }
 }
